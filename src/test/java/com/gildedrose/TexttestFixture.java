94517e9b08b628ef21984b28f8da1b20ac53a0f2
Maciej <maciek6001@gmail.com>
2019-11-13 08:20:07 +0100

    feat: homework final version

--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -18,7 +18,7 @@ public class TexttestFixture {
 
         GildedRose app = new GildedRose(items);
 
-        int days = 20;
+        int days = 120;
         if (args.length > 0) {
             days = Integer.parseInt(args[0]) + 1;
         }
