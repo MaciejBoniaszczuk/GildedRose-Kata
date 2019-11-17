abd5d18003ab32e23390e9c25721f3bf91bc353a
Maciej <maciek6001@gmail.com>
2019-11-17 19:14:15 +0100

    feat: unused imports deleted

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,8 +1,6 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
 
-import org.approvaltests.Approvals;
 import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.Test;
 
